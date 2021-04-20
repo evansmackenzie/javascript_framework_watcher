94763f87fa9ce1ce53af98ad334a6e9b992c8361
mackenzie <evans.mackenzie@gmail.com>
2021-04-20 16:48:50 -0400

    changes watchers_count to subscribers_count in api axios requests

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,23 +5,23 @@
     </div>
     <h1>vuejs repo</h1>
     <p>stargazers count: {{ vue_stargazers_count }}</p>
-    <p>watchers count: {{ vue_watchers_count }}</p>
+    <p>subscribers count: {{ vue_subscribers_count }}</p>
     <p>forks count: {{ vue_forks_count }}</p>
     <h1>angular repo</h1>
     <p>stargazers count: {{ angular_stargazers_count }}</p>
-    <p>watchers count: {{ angular_watchers_count }}</p>
+    <p>subscribers count: {{ angular_subscribers_count }}</p>
     <p>forks count: {{ angular_forks_count }}</p>
     <h1>ember repo</h1>
     <p>stargazers count: {{ ember_stargazers_count }}</p>
-    <p>watchers count: {{ ember_watchers_count }}</p>
+    <p>subscribers count: {{ ember_subscribers_count }}</p>
     <p>forks count: {{ ember_forks_count }}</p>
     <h1>svelte repo</h1>
     <p>stargazers count: {{ svelte_stargazers_count }}</p>
-    <p>watchers count: {{ svelte_watchers_count }}</p>
+    <p>subscribers count: {{ svelte_subscribers_count }}</p>
     <p>forks count: {{ svelte_forks_count }}</p>
     <h1>react repo</h1>
     <p>stargazers count: {{ react_stargazers_count }}</p>
-    <p>watchers count: {{ react_watchers_count }}</p>
+    <p>subscribers count: {{ react_subscribers_count }}</p>
     <p>forks count: {{ react_forks_count }}</p>
   </div>
 </template>
@@ -41,19 +41,19 @@ export default {
   data: function () {
     return {
       vue_stargazers_count: "",
-      vue_watchers_count: "",
+      vue_subscribers_count: "",
       vue_forks_count: "",
       angular_stargazers_count: "",
-      angular_watchers_count: "",
+      angular_subscribers_count: "",
       angular_forks_count: "",
       ember_stargazers_count: "",
-      ember_watchers_count: "",
+      ember_subscribers_count: "",
       ember_forks_count: "",
       svelte_stargazers_count: "",
-      svelte_watchers_count: "",
+      svelte_subscribers_count: "",
       svelte_forks_count: "",
       react_stargazers_count: "",
-      react_watchers_count: "",
+      react_subscribers_count: "",
       react_forks_count: "",
       chartOptions: {
         chart: {
@@ -106,7 +106,7 @@ export default {
             data: [],
           },
           {
-            name: "Watchers Count",
+            name: "Subscribers (Watchers) Count",
             data: [],
           },
           {
@@ -121,10 +121,10 @@ export default {
     axios.get("https://api.github.com/repos/vuejs/vue").then((response) => {
       console.log(response.data);
       this.vue_stargazers_count = response.data["stargazers_count"];
-      this.vue_watchers_count = response.data["watchers_count"];
+      this.vue_subscribers_count = response.data["subscribers_count"];
       this.vue_forks_count = response.data["forks_count"];
       this.chartOptions.series[0].data = [this.vue_stargazers_count];
-      this.chartOptions.series[1].data = [this.vue_watchers_count];
+      this.chartOptions.series[1].data = [this.vue_subscribers_count];
       this.chartOptions.series[2].data = [this.vue_forks_count];
     });
     axios
@@ -132,10 +132,10 @@ export default {
       .then((response) => {
         console.log(response.data);
         this.angular_stargazers_count = response.data["stargazers_count"];
-        this.angular_watchers_count = response.data["watchers_count"];
+        this.angular_subscribers_count = response.data["subscribers_count"];
         this.angular_forks_count = response.data["forks_count"];
         this.chartOptions.series[0].data.push(this.angular_stargazers_count);
-        this.chartOptions.series[1].data.push(this.angular_watchers_count);
+        this.chartOptions.series[1].data.push(this.angular_subscribers_count);
         this.chartOptions.series[2].data.push(this.angular_forks_count);
       });
     axios
@@ -143,10 +143,10 @@ export default {
       .then((response) => {
         console.log(response.data);
         this.ember_stargazers_count = response.data["stargazers_count"];
-        this.ember_watchers_count = response.data["watchers_count"];
+        this.ember_subscribers_count = response.data["subscribers_count"];
         this.ember_forks_count = response.data["forks_count"];
         this.chartOptions.series[0].data.push(this.ember_stargazers_count);
-        this.chartOptions.series[1].data.push(this.ember_watchers_count);
+        this.chartOptions.series[1].data.push(this.ember_subscribers_count);
         this.chartOptions.series[2].data.push(this.ember_forks_count);
       });
     axios
@@ -154,10 +154,10 @@ export default {
       .then((response) => {
         console.log(response.data);
         this.svelte_stargazers_count = response.data["stargazers_count"];
-        this.svelte_watchers_count = response.data["watchers_count"];
+        this.svelte_subscribers_count = response.data["subscribers_count"];
         this.svelte_forks_count = response.data["forks_count"];
         this.chartOptions.series[0].data.push(this.svelte_stargazers_count);
-        this.chartOptions.series[1].data.push(this.svelte_watchers_count);
+        this.chartOptions.series[1].data.push(this.svelte_subscribers_count);
         this.chartOptions.series[2].data.push(this.svelte_forks_count);
       });
     axios
@@ -165,10 +165,10 @@ export default {
       .then((response) => {
         console.log(response.data);
         this.react_stargazers_count = response.data["stargazers_count"];
-        this.react_watchers_count = response.data["watchers_count"];
+        this.react_subscribers_count = response.data["subscribers_count"];
         this.react_forks_count = response.data["forks_count"];
         this.chartOptions.series[0].data.push(this.react_stargazers_count);
-        this.chartOptions.series[1].data.push(this.react_watchers_count);
+        this.chartOptions.series[1].data.push(this.react_subscribers_count);
         this.chartOptions.series[2].data.push(this.react_forks_count);
       });
   },
