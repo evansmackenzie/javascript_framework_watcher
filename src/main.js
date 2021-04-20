3b91e250b6d28a7d85fb0be8e9ad90d273a80f96
mackenzie <evans.mackenzie@gmail.com>
2021-04-20 13:05:31 -0400

    installs highcharts-vue js library, an displays bar graph on home vue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import HighchartsVue from 'highcharts-vue';
+
+
+Vue.use(HighchartsVue);
 
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 // axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://api.github.com/repos" : "/";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <div>
+      <highcharts class="hc" :options="chartOptions" ref="chart"></highcharts>
+    </div>
     <h1>vuejs repo</h1>
     <p>stargazers count: {{ vue_stargazers_count }}</p>
     <p>watchers count: {{ vue_watchers_count }}</p>
@@ -27,9 +30,13 @@
 // @ is an alias to /src
 
 import axios from "axios";
+import { Chart } from "highcharts-vue";
 
 export default {
   name: "Home",
+  components: {
+    highcharts: Chart,
+  },
 
   data: function () {
     return {
@@ -48,6 +55,66 @@ export default {
       react_stargazers_count: "",
       react_watchers_count: "",
       react_forks_count: "",
+      chartOptions: {
+        chart: {
+          type: "bar",
+        },
+        title: {
+          text: "JavaScript Framework Watcher",
+        },
+
+        xAxis: {
+          categories: ["VueJS", "Angular", "Ember", "Svelte", "React"],
+          title: {
+            text: null,
+          },
+        },
+        yAxis: {
+          min: 0,
+
+          labels: {
+            overflow: "justify",
+          },
+        },
+        tooltip: {
+          valueSuffix: " millions",
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: true,
+            },
+          },
+        },
+        legend: {
+          layout: "vertical",
+          align: "right",
+          verticalAlign: "top",
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor: "#FFFFFF",
+          shadow: true,
+        },
+        credits: {
+          enabled: false,
+        },
+        series: [
+          {
+            name: "Stargazers Count",
+            data: [],
+          },
+          {
+            name: "Watchers Count",
+            data: [],
+          },
+          {
+            name: "Forks Count",
+            data: [],
+          },
+        ],
+      },
     };
   },
   created: function () {
@@ -56,6 +123,9 @@ export default {
       this.vue_stargazers_count = response.data["stargazers_count"];
       this.vue_watchers_count = response.data["watchers_count"];
       this.vue_forks_count = response.data["forks_count"];
+      this.chartOptions.series[0].data = [this.vue_stargazers_count];
+      this.chartOptions.series[1].data = [this.vue_watchers_count];
+      this.chartOptions.series[2].data = [this.vue_forks_count];
     });
     axios
       .get("https://api.github.com/repos/angular/angular.js")
@@ -64,6 +134,9 @@ export default {
         this.angular_stargazers_count = response.data["stargazers_count"];
         this.angular_watchers_count = response.data["watchers_count"];
         this.angular_forks_count = response.data["forks_count"];
+        this.chartOptions.series[0].data.push(this.angular_stargazers_count);
+        this.chartOptions.series[1].data.push(this.angular_watchers_count);
+        this.chartOptions.series[2].data.push(this.angular_forks_count);
       });
     axios
       .get("https://api.github.com/repos/emberjs/ember.js")
@@ -72,6 +145,9 @@ export default {
         this.ember_stargazers_count = response.data["stargazers_count"];
         this.ember_watchers_count = response.data["watchers_count"];
         this.ember_forks_count = response.data["forks_count"];
+        this.chartOptions.series[0].data.push(this.ember_stargazers_count);
+        this.chartOptions.series[1].data.push(this.ember_watchers_count);
+        this.chartOptions.series[2].data.push(this.ember_forks_count);
       });
     axios
       .get("https://api.github.com/repos/sveltejs/svelte")
@@ -80,6 +156,9 @@ export default {
         this.svelte_stargazers_count = response.data["stargazers_count"];
         this.svelte_watchers_count = response.data["watchers_count"];
         this.svelte_forks_count = response.data["forks_count"];
+        this.chartOptions.series[0].data.push(this.svelte_stargazers_count);
+        this.chartOptions.series[1].data.push(this.svelte_watchers_count);
+        this.chartOptions.series[2].data.push(this.svelte_forks_count);
       });
     axios
       .get("https://api.github.com/repos/facebook/react")
@@ -88,6 +167,9 @@ export default {
         this.react_stargazers_count = response.data["stargazers_count"];
         this.react_watchers_count = response.data["watchers_count"];
         this.react_forks_count = response.data["forks_count"];
+        this.chartOptions.series[0].data.push(this.react_stargazers_count);
+        this.chartOptions.series[1].data.push(this.react_watchers_count);
+        this.chartOptions.series[2].data.push(this.react_forks_count);
       });
   },
 };
